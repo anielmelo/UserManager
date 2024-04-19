69d4a501b2c73501b3779bf652449aada6c24bb9
anielmelo <anielcontato@gmail.com>
2024-04-19 12:48:23 -0300

    fix: fix cors headers

--- api/src/Http/Response.php
+++ api/src/Http/Response.php
@@ -5,7 +5,9 @@ namespace App\Http;
 class Response {
     public static function json(array $data = [], int $status = 200) {
         http_response_code($status);
-        header('Content-type: application/json');
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Access-Control-Allow-Headers: *");
         echo json_encode($data);
     }
 }
