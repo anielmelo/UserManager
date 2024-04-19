e4239d7b4d0283f0400bb3819efd3300d1210e14
anielmelo <anielcontato@gmail.com>
2024-04-19 12:50:57 -0300

    fix: fix response type

--- api/src/Http/Response.php
+++ api/src/Http/Response.php
@@ -5,6 +5,7 @@ namespace App\Http;
 class Response {
     public static function json(array $data = [], int $status = 200) {
         http_response_code($status);
+        header('Content-type: application/json');
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
         header("Access-Control-Allow-Headers: *");
