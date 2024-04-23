2acf1b1dddb8a15cdd055e0ed61005d1e7a171ce
anielmelo <anielcontato@gmail.com>
2024-04-23 12:12:07 -0300

    feat: update cors

--- api/index.php
+++ api/index.php
@@ -11,6 +11,9 @@ if (file_exists(__DIR__ . '/.env')) {
     $dotenv = Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 }
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+header("Access-Control-Allow-Methods: GET,OPTIONS,PATCH,DELETE,POST,PUT");
+header("Access-Control-Allow-Credentials: true");
 
 Core::dispatch(Route::routes());
--- api/src/Http/Response.php
+++ api/src/Http/Response.php
@@ -6,9 +6,6 @@ class Response {
     public static function json(array $data = [], int $status = 200) {
         http_response_code($status);
         header('Content-type: application/json');
-        header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-        header("Access-Control-Allow-Headers: *");
         echo json_encode($data);
     }
 }
