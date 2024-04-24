f2d8df852a830abf75c37105ee3d97a1641f9adc
anielmelo <anielcontato@gmail.com>
2024-04-24 14:55:29 -0300

    fix: response method

--- api/src/Http/Response.php
+++ api/src/Http/Response.php
@@ -8,7 +8,7 @@ class Response {
             http_response_code(200);
     
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-                header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
+                header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
             }
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
                 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding");
